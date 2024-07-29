73c8ae2be39bc54bb2e1b10e3cdfb183e0bd94d6
requious <tehrequious@gmail.com>
2024-07-29 17:53:56 -0400

    set up postgres connection

--- lib/postgres/postgres.go
+++ lib/postgres/postgres.go
@@ -1,5 +1,57 @@
 package postgres
 
-type Postgres struct {
+import (
+	"database/sql"
+	"fmt"
 
+	_ "github.com/lib/pq"
+)
+
+type Postgres struct {
+	Db *sql.DB
+}
+
+func NewPostgres(connStr string) (*Postgres, error) {
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, fmt.Errorf("failed to initialize Postgres connection: %s", err)
+	}
+	return &Postgres{
+		Db: db,
+	}, nil
+}
+
+type ItemForRead struct {
+	ItemID                *sql.NullInt32
+	Type                  *sql.NullString
+	Name                  *sql.NullString
+	ItemLevel             *sql.NullInt32
+	SpecialCurrencyItemID *sql.NullInt32
+	SpecialCurrencyCount  *sql.NullInt32
+	HighQualityable       *sql.NullBool
+	Marketable            *sql.NullBool
+	GilPrice              *sql.NullInt32
+	ClassJobRestriction   *sql.NullString
+}
+
+func (p *Postgres) SelectItemWithID(id int32) (*ItemForRead, error) {
+	i := ItemForRead{
+		ItemID:                &sql.NullInt32{},
+		Type:                  &sql.NullString{},
+		Name:                  &sql.NullString{},
+		ItemLevel:             &sql.NullInt32{},
+		SpecialCurrencyItemID: &sql.NullInt32{},
+		SpecialCurrencyCount:  &sql.NullInt32{},
+		HighQualityable:       &sql.NullBool{},
+		Marketable:            &sql.NullBool{},
+		GilPrice:              &sql.NullInt32{},
+		ClassJobRestriction:   &sql.NullString{},
+	}
+
+	row := p.Db.QueryRow("SELECT * FROM items WHERE item_id = $1", id)
+
+	if err := row.Scan(i.ItemID, i.Type, i.Name, i.ItemLevel, i.SpecialCurrencyItemID, i.SpecialCurrencyCount, i.HighQualityable, i.Marketable, i.GilPrice, i.ClassJobRestriction); err != nil {
+		return nil, fmt.Errorf("failed to retrieve row for item %v: %w", id, err)
+	}
+	return &i, nil
 }
--- profiteeringway.go
+++ profiteeringway.go
@@ -1,7 +1,28 @@
 package main
 
-
+import (
+	"fmt"
+	"profiteeringway/lib/postgres"
+	"profiteeringway/secrets"
+)
 
 func main() {
-	
+	pg, err := postgres.NewPostgres(secrets.PostgresConnectionString)
+	if err != nil {
+		fmt.Printf("failed to initialize postgres: %v\n", err)
+		return
+	}
+
+	item, err := pg.SelectItemWithID(31831)
+	if err != nil {
+		fmt.Printf("failed to retrieve item: %v\n", err)
+		return
+	}
+
+	name, err := item.Name.Value()
+	if err != nil {
+		fmt.Printf("failed to scan name from item: %v\n", err)
+	}
+
+	fmt.Printf("got item %s", name)
 }
