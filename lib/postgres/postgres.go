1d5485829ad40f362e1982ea1b7d886f7876f18a
requious <tehrequious@gmail.com>
2024-07-31 08:48:47 -0400

    complete first verison for polling testing

--- lib/postgres/postgres.go
+++ lib/postgres/postgres.go
@@ -210,10 +210,11 @@ func (p *Postgres) DawntrailTierOneCraftedEquipment() ([]int, error) {
 	return p.GetItemIDsForStaticQuery(`SELECT item_id FROM items WHERE type IN ('Marauder''s Arm','Two–handed Thaumaturge''s Arm','Weaver''s Primary Tool','Goldsmith''s Secondary Tool','Botanist''s Secondary Tool','Astrologian''s Arm','Fisher''s Primary Tool','Alchemist''s Primary Tool','Archer''s Arm','One–handed Conjurer''s Arm','Blacksmith''s Primary Tool','Arcanist''s Grimoire','Goldsmith''s Primary Tool','Alchemist''s Secondary Tool','Gladiator''s Arm','Red Mage''s Arm','Leatherworker''s Primary Tool','Scholar''s Arm','Earrings','Sage''s Arm','Blue Mage''s Arm','Rogue''s Arm','Blacksmith''s Secondary Tool','Culinarian''s Primary Tool','Reaper''s Arm','Miner''s Secondary Tool','Botanist''s Primary Tool','Culinarian''s Secondary Tool','Weaver''s Secondary Tool','Dancer''s Arm','Carpenter''s Secondary Tool','Armorer''s Primary Tool','Carpenter''s Primary Tool','Two–handed Conjurer''s Arm','Armorer''s Secondary Tool','One–handed Thaumaturge''s Arm','Dark Knight''s Arm','Miner''s Primary Tool','Samurai''s Arm','Shield','Fisher''s Secondary Tool','Machinist''s Arm','Hands','Body','Head','Necklace','Ring','Legs','Feet','Bracelets','Leatherworker''s Secondary Tool','Pugilist''s Arm','Lancer''s Arm','Pictomancer''s Arm','Viper''s Arm','Gunbreaker''s Arm') AND item_level > 709 AND marketable`)
 }
 
-/*
-'Reagent','Ingredient','Cloth','Seafood','Crystal'
+func (p *Postgres) DawntrailMaterialsSetOne() ([]int, error) {
+	return p.GetItemIDsForStaticQuery(`SELECT item_id FROM items WHERE type IN ('Reagent','Ingredient','Seafood','Crystal','Metal','Stone','Lumber','Bone') AND item_level > 679 AND marketable;`)
+}
 
-split to avoid 100+
-
-'Metal','Bone','Leather','Stone','Lumber'
-*/
+// We split these up to stay below the 100 item threshold for Universalis data.
+func (p *Postgres) DawntrailMaterialsSetTwo() ([]int, error) {
+	return p.GetItemIDsForStaticQuery(`SELECT name FROM items WHERE type IN ('Leather','Cloth') AND item_level > 700;`)
+}
--- profiteeringway.go
+++ profiteeringway.go
@@ -11,7 +11,15 @@ import (
 	"time"
 )
 
-func MakeHotlist(worldIDs []int, itemIDs []int, name string) *hotlist.Hotlist {
+const (
+	HotlistDawntrailMateria                 = "Dawntrail Materia"
+	HotlistDawntrailConsumables             = "Dawntrail Consumables"
+	HotlistDawntrailTierOneCraftedEquipment = "Dawntrail Tier One Crafted Equipment"
+	HotlistDawntrailMaterialsSetOne         = "Dawntrail Materials (Set One)"
+	HotlistDawntrailMaterialsSetTwo         = "Dawntrail Materials (Set Two)"
+)
+
+func makeHotlist(worldIDs []int, itemIDs []int, name string) *hotlist.Hotlist {
 	fifteenMinutes, err := time.ParseDuration("15m")
 	if err != nil {
 		panic(fmt.Sprintf("failed to parse duration: %s", err))
@@ -23,6 +31,56 @@ func MakeHotlist(worldIDs []int, itemIDs []int, name string) *hotlist.Hotlist {
 		PollFrequency: fifteenMinutes,
 		WorldIDs:      worldIDs,
 	}
+}
+
+func copyIntSlice(s []int) []int {
+	c := make([]int, len(s))
+
+	copy(c, s)
+
+	return c
+}
+
+func dawntrailTierOneHotlists(p *postgres.Postgres) ([]*hotlist.Hotlist, error) {
+	var ret []*hotlist.Hotlist
+
+	worldIDs, err := p.NorthAmericanWorlds()
+	if err != nil {
+		return nil, fmt.Errorf("%w", err)
+	}
+
+	materiaIDs, err := p.DawntrailMateriaIDs()
+	if err != nil {
+		return nil, fmt.Errorf("%w", err)
+	}
+
+	consumableIDs, err := p.DawntrailConsumables()
+	if err != nil {
+		return nil, fmt.Errorf("%w", err)
+	}
+
+	craftedIDs, err := p.DawntrailTierOneCraftedEquipment()
+	if err != nil {
+		return nil, fmt.Errorf("%w", err)
+	}
+
+	materialsOneID, err := p.DawntrailMaterialsSetOne()
+	if err != nil {
+		return nil, fmt.Errorf("%w", err)
+	}
+
+	materialsTwoID, err := p.DawntrailMaterialsSetTwo()
+	if err != nil {
+		return nil, fmt.Errorf("%w", err)
+	}
+
+	ret = append(ret, makeHotlist(copyIntSlice(worldIDs), materiaIDs, HotlistDawntrailMateria))
+	ret = append(ret, makeHotlist(copyIntSlice(worldIDs), consumableIDs, HotlistDawntrailConsumables))
+	ret = append(ret, makeHotlist(copyIntSlice(worldIDs), craftedIDs, HotlistDawntrailTierOneCraftedEquipment))
+	ret = append(ret, makeHotlist(copyIntSlice(worldIDs), materialsOneID, HotlistDawntrailMaterialsSetOne))
+	ret = append(ret, makeHotlist(copyIntSlice(worldIDs), materialsTwoID, HotlistDawntrailMaterialsSetTwo))
+
+	return ret, nil
 }
 
 func main() {
@@ -38,8 +96,23 @@ func main() {
 
 	hub := hotlist.NewHotlistHub(pg)
 
+	hotlists, err = dawntrailTierOneHotlists(pg)
+	if err != nil {
+		panic(fmt.Sprintf("%s", err))
+	}
+
+	hub.ConfiguredHotlists = make(map[string]*hotlist.Hotlist)
+	for _, hotlist := range hotlists {
+		hub.ConfiguredHotlists[hotlist.Name] = hotlist
+	}
+
+	if err := hub.BeginPollingAll(); err != nil {
+		panic(fmt.Sprintf("%s", err))
+	}
+
 	sigStopChan := make(chan os.Signal, 1)
 	signal.Notify(sigStopChan, syscall.SIGTSTP)
+	signal.Notify(sigStopChan, syscall.SIGINT)
 	for {
 		<-sigStopChan
 		hub.CleanUp()
